5052c8af806ac4a54cfcdecea248686091762063
basarat <basaratali@gmail.com>
2016-03-28 19:40:54 +1100

    react

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: './src/app.tsx',
+    output: {
+        filename: './dist/bundle.js'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
+    }
+}
