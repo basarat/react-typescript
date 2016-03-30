403b0e54cae3e6de4395a3a0f4b249918ebccd56
Basarat Syed <basaratali@gmail.com>
2016-03-30 13:39:44 +1100

    stateless

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-class Hello extends React.Component<{ compiler: string, framework: string }, {}> {
-    render() {
-        return (
-            <div>
-                <div>{this.props.compiler}</div>
-                <div>{this.props.framework}</div>
-            </div>
-        );
-    }
+const Hello = (props: { compiler: string, framework: string }) => {
+    return (
+        <div>
+            <div>{props.compiler}</div>
+            <div>{props.framework}</div>
+        </div>
+    );
 }
 
 ReactDOM.render(
