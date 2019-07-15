b4cf4546118c30d547307a57f418234ca73c079c
Basarat <basaratali@gmail.com>
2019-07-15 15:13:16 +1000

    upgrade to webpack latest :rose: closes https://github.com/basarat/react-typescript/issues/7

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app/app.tsx',
   plugins: [
     new CleanWebpackPlugin({
-	  cleanAfterEveryBuildPatterns:['public/build']
-	}),
+      cleanAfterEveryBuildPatterns: ['public/build']
+    }),
     new HtmlWebpackPlugin({
       template: 'src/templates/index.html'
     }),
