59af6a37455eba4a666cf9e1020ec4ba79fc0cae
Basarat <basaratali@gmail.com>
2020-03-18 12:26:14 +1100

    wip

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Counter, useCounter } from '../components/counter';
 
-function useAppState() {
-  const counterOne = useCounter();
+function useApp() {
   return {
-    counterOne,
   }
 }
 
 const App: React.FC = () => {
-  const appState = useAppState();
+  const appState = useApp();
   return (
     <div>
       <h1>Hello</h1>
-      <Counter use={appState.counterOne} />
     </div>
   );
 }
